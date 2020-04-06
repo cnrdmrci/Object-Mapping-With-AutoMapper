e93496faf493f37f14cbb6e435f5a3d904164ac1
cnrdmrci <canerdemirci.mail@gmail.com>
2020-04-07 00:53:47 +0300

    Ana sınıf eklendi.

---
+++ Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectMappingWithAutoMapper
+{
+    class Order
+    {
+        public int OrderId { get; set; }
+        public string UrunAdi { get; set; }
+        public string UrunGrubu { get; set; }
+        public int FabrikaNo { get; set; }
+        public double Agirlik { get; set; }    
+        public Guid IslemNo { get; set; }
+        public decimal Fiyat { get; set; }
+        public DateTime SiparisTarihi { get; set; }
+        public DateTime GonderimTarihi { get; set; }
+    }
+}
