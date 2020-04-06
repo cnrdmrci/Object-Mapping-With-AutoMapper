58f5698d9a0db99e87e8a7b7b8ff306b37bf5b23
cnrdmrci <canerdemirci.mail@gmail.com>
2020-04-07 00:54:00 +0300

    Dto sınıfı eklendi.

---
+++ OrderDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectMappingWithAutoMapper
+{
+    class OrderDTO
+    {
+        public int SiparisNumarasi { get; set; }
+        public decimal ToplamTutar { get; set; }
+        public DateTime SiparisTarihi { get; set; }
+        public override string ToString()
+        {
+            return "Siparis No: " + SiparisNumarasi + ", Tarih : " + SiparisTarihi + ", Tutar : " + ToplamTutar;
+        }   
+    }
+}
