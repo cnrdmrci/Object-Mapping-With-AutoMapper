fb5c3bc590d60859d695fe831ff22a3ed32435a8
cnrdmrci <canerdemirci.mail@gmail.com>
2020-04-07 00:54:58 +0300

    AutoMapper, auto mapping işlemi yapıldı.

---
+++ OrderEngine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+
+namespace ObjectMappingWithAutoMapper
+{
+    public class OrderEngine
+    {
+        private readonly IMapper _mapper;
+        public OrderEngine(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public void Run()
+        {
+            Order order = new Order()
+            {
+                OrderId = 123,
+                UrunAdi = "Telefon",
+                UrunGrubu = "Teknoloji",
+                FabrikaNo = 556265,
+                Agirlik = 0.2,
+                IslemNo = new Guid(),
+                Fiyat = 5000,
+                SiparisTarihi = DateTime.Today,
+                GonderimTarihi = DateTime.Today
+            };
+
+            OrderDTO dest = _mapper.Map<Order, OrderDTO>(order);
+
+            Console.WriteLine(dest.ToString());
+        }
+    }
+}
